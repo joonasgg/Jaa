6f96f99bed89e4b4f528ccf00ab25bfd56cfbaf9
joonasgg <jjkp117@gmail.com>
2016-01-19 17:30:14 +0200

    Initial commit.

---
+++ Jaa/Jaa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1;
+            Console.Write("Anna luku 1");
+            luku1 = int.Parse(Console.ReadLine());
+            int luku2;
+            Console.Write("Anna luku 2");
+            luku2 = int.Parse(Console.ReadLine());
+            int luku3;
+            Console.Write("Anna luku 3");
+            luku3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lukujen summa on" + (luku1+luku2+luku3));
+        }
+    }
+}
